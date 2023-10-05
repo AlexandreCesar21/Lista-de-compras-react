d998f5bb9222077be2d1afd059f429d91da5399c
Alexandre César Pereira de Barros Filho <xandebarros9@gmail.com>
2023-10-05 15:00:21 -0300

    Ultimas atualizações

--- lista-compras/src/pages/Redes.js
+++ lista-compras/src/pages/Redes.js
@@ -5,24 +5,27 @@ import estilo from "./Redes.module.css"
 
 function Redes() {
     return (
-        <div className={estilo.seguencia}>
-            <div>  
-            <a href="https://www.linkedin.com/in/alexandre-c%C3%A9sar-350726256/">
-            <img src={linkedin} style={{height: "137px"}}/>
-            </a>
-            <h1>Linkdin</h1>
-            </div>
-            <div>
-            <a href="https://www.instagram.com/alexandrecesar9477/">
-            <img src={instagram}  style={{height: "137px"}}/>
-            </a>
-                <h1>Instagram</h1>
-            </div>
-            <div>
-            <a href="https://github.com/AlexandreCesar21">
-            <img src={github} style={{height: "137px"}}/>
-            </a>
-            <h1>GitHub</h1>
+        <div>
+            <div className={estilo.titulo}><h1>Minhas Redes Sociais</h1></div>
+            <div className={estilo.seguencia}>
+                <div className={estilo.caixa}>  
+                <a href="https://www.linkedin.com/in/alexandre-c%C3%A9sar-350726256/" target="_blank" rel="external">
+                <img src={linkedin} style={{height: "137px"}}/>
+                </a>
+                <h1>Linkdin</h1>
+                </div>
+                <div className={estilo.caixa}>
+                <a href="https://www.instagram.com/alexandrecesar9477/" target="_blank" rel="external">
+                <img src={instagram}  style={{height: "137px"}}/>
+                </a>
+                    <h1>Instagram</h1>
+                </div>
+                <div className={estilo.caixa}>
+                <a href="https://github.com/AlexandreCesar21" target="_blank" rel="external">
+                <img src={github} style={{height: "137px"}}/>
+                </a>
+                <h1>GitHub</h1>
+                </div>
             </div>
         </div>
     )
--- lista-compras/src/pages/Tecnologia.js
+++ lista-compras/src/pages/Tecnologia.js
@@ -1,5 +1,31 @@
+import estilo from "./Tecnologia.module.css"
+
 function Tecnologia() {
-    return <h1>Tecnologia</h1>
+    return (
+        <div>
+            <div className={estilo.titulo}>
+                <h1>Tecnologias utilizadas para o projeto!</h1>
+            </div>
+            <div className={estilo.seguencia}>
+                <div>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" style={{height: "97px"}}/>
+                <h1>HTML</h1>
+                </div>
+                <div>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" style={{height: "97px"}}/>
+                <h1>CSS</h1>
+                </div>
+                <div>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" style={{height: "97px"}}/>
+                <h1>JavaScript</h1>
+                </div>
+                <div>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" style={{height: "97px"}}/>
+                <h1>React.JS</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Tecnologia
